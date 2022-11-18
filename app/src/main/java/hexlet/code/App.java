7be09469e3c9b15797de1ba63954f71c6de9ce38
Andrew <rassandev@gmail.com>
2022-11-18 15:41:52 +0300

    fix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,21 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        var choice = scanner.nextInt();
+        System.out.println("Your choice: " + choice);
+
+        if (choice == 2) {
+            Even.game();
+        }
+        scanner.close();
     }
 }
