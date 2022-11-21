bbc324584589adbf8f4ba62d74a757835abff6e7
Andrew <rassandev@gmail.com>
2022-11-21 17:09:54 +0300

    fix App.java and Even.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,8 @@
 package hexlet.code;
 
+import hexlet.code.games.Calc;
+import hexlet.code.games.Even;
+
 import java.util.Scanner;
 
 public class App {
@@ -9,13 +12,25 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
-        var choice = scanner.nextInt();
+        var choice = scanner.next();
         System.out.println("Your choice: " + choice);
 
-        if (choice == 2) {
-            Even.game();
+        switch(choice) {
+            case "0":
+                break;
+            case "1":
+                Cli.greeting();
+                break;
+            case "2":
+                Even.game();
+                break;
+            case "3":
+                Calc.game();
+                break;
         }
+
         scanner.close();
     }
 }
---
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -0,0 +1,28 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import java.util.Random;
+
+public class Even {
+    private static final String RULES = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+    public static void game() {
+        Random random = new Random();
+
+        String[][] roundsData = new String[Engine.ROUNDS_COUNT][2];
+
+
+        for (var i = 0; i < Engine.ROUNDS_COUNT; i++) {
+            var generatedNumber = random.nextInt(100);
+
+            roundsData[i][0] = String.valueOf(generatedNumber);
+
+            if (generatedNumber % 2 == 0) {
+                roundsData[i][1] = "yes";
+            } else {
+                roundsData[i][1] = "no";
+            }
+        }
+
+        Engine.runGame(RULES, roundsData);
+    }
+}
