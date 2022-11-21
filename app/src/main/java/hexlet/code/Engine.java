5816357cc78b880d9b1165f90f167b8576e27941
Andrew <rassandev@gmail.com>
2022-11-21 17:12:30 +0300

    add Engine.java

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,46 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+    public static final int ROUNDS_COUNT = 3;
+
+    public static void runGame(String rules, String[][] gameData) {
+        Scanner scanner = new Scanner(System.in);
+
+        var userName = "";
+        var userAnswer = "";
+        var answersCount = 0;
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        System.out.println(rules);
+        System.out.println("To win, you need to give three correct answers in a row.");
+        System.out.println();
+
+        for (var i = 0; i < ROUNDS_COUNT; i++) {
+            System.out.println("Question: " + gameData[i][0]);
+            userAnswer = scanner.next();
+            System.out.println("Your answer: " + userAnswer);
+
+            if (userAnswer.equalsIgnoreCase(gameData[i][1])) {
+                System.out.println("Correct!");
+                answersCount++;
+                System.out.println("Correct answers: " + answersCount);
+                System.out.println();
+                if (answersCount == 3) {
+                    System.out.println("Congratulations, " + userName + "!");
+                }
+            } else {
+                System.out.println("'" + userAnswer + "'" + " is wrong answer ;(. Correct answer was "
+                        + "'" + gameData[i][1] + "'.");
+                System.out.println("Let's try again, " + userName + "!");
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+}
