413fddc24a88eadc7436225f560f47520245deab
Andrew <rassandev@gmail.com>
2022-11-24 17:44:17 +0300

    fix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,10 @@
 package hexlet.code;
 
-import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.Calc;
 import hexlet.code.games.Gcd;
 import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
 
 import java.util.Scanner;
 
@@ -17,6 +18,7 @@ public class App {
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
         var choice = scanner.next();
         System.out.println("Your choice: " + choice);
@@ -38,6 +40,9 @@ public class App {
                 break;
             case "5":
                 Progression.game();
+                break;
+            case "6":
+                Prime.game();
                 break;
             default:
                 System.out.println("Selected option doesn't exist :(");
