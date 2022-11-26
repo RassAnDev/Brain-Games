69024c2b1cf4e67d8539a06c3c1797b8b3ecf62b
Andrew <rassandev@gmail.com>
2022-11-26 19:47:38 +0300

    fix Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,13 +1,18 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Utils;
 
-import java.util.Random;
 
 public class Prime {
+    private static final int MIN_VALUE = -10;
+    private static final int MAX_VALUE = 100;
     private static final String RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
 
     private static boolean isPrime(int number) {
+        if (number < 1) {
+            return false;
+        }
         for (var i = 2; i * i <= number; i++) {
             if (number % i == 0) {
                 return false;
@@ -16,19 +21,12 @@ public class Prime {
         return true;
     }
     public static void game() {
-        Random random = new Random();
-
         String[][] roundsData = new String[Engine.ROUNDS_COUNT][2];
 
         for (var i = 0; i < Engine.ROUNDS_COUNT; i++) {
-            var generatedNumber = random.nextInt(100) + 2;
+            var generatedNumber = Utils.generateNumber(MIN_VALUE, MAX_VALUE);
             roundsData[i][0] = String.valueOf(generatedNumber);
-
-            if (isPrime(generatedNumber)) {
-                roundsData[i][1] = "yes";
-            } else {
-                roundsData[i][1] = "no";
-            }
+            roundsData[i][1] = isPrime(generatedNumber) ? "yes" : "no";
         }
 
         Engine.runGame(RULES, roundsData);
