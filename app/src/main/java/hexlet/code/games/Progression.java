21c801d6c016c1dbe1397874a649acb388f0fb42
Andrew <rassandev@gmail.com>
2022-11-26 19:48:35 +0300

    fix Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,27 +1,35 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-
-import java.util.Random;
+import hexlet.code.Utils;
 
 public class Progression {
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 100;
     private static final String RULES = "What number is missing in the progression?";
 
-    public static void game() {
-        Random random = new Random();
+    private static String[] makeProgression(int start, int step, int length) {
+        String[] progression = new String[length];
 
+        for (int i = 0; i < length; i++) {
+            progression[i] = Integer.toString(start + i * step);
+        }
+
+        return progression;
+    }
+
+    public static void game() {
         String[][] roundsData = new String[Engine.ROUNDS_COUNT][2];
 
         for (var i = 0; i < Engine.ROUNDS_COUNT; i++) {
-            var startProgression = random.nextInt(100) + 1;
-            var stepProgression = random.nextInt(10);
-            var sizeProgression = random.nextInt(5 + 1) + 5;
-            var randomElement = random.nextInt(sizeProgression);
+            var startProgression = Utils.generateNumber(MIN_VALUE, MAX_VALUE);
+            var stepProgression = Utils.generateNumber(1, 10);
+            var lengthProgression = Utils.generateNumber(5, 10);
+            var randomElement = Utils.generateNumber(MIN_VALUE, lengthProgression - 1);
             var hiddenElement = "";
-            String[] progression = new String[sizeProgression];
+            String[] progression = makeProgression(startProgression, stepProgression, lengthProgression);
 
-            for (var j = 0; j < sizeProgression; j++) {
-                progression[j] = String.valueOf(startProgression + ((j - 1) * stepProgression));
+            for (var j = 0; j < progression.length; j++) {
                 if (j == randomElement) {
                     hiddenElement = progression[j];
                     progression[j] = "..";
