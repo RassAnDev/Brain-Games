2da86a367b4bea3847a48313c8cca09cf495a4d4
Andrew <rassandev@gmail.com>
2022-11-26 20:04:10 +0300

    fix Gcd.java

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -1,10 +1,11 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-
-import java.util.Random;
+import hexlet.code.Utils;
 
 public class Gcd {
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 100;
     private static final String RULES = "Find the greatest common divisor of given numbers.";
 
     private static int calculateGcd(int firstNumber, int secondNumber) {
@@ -15,13 +16,11 @@ public class Gcd {
     }
 
     public static void game() {
-        Random random = new Random();
-
         String[][] roundsData = new String[Engine.ROUNDS_COUNT][2];
 
         for (var i = 0; i < Engine.ROUNDS_COUNT; i++) {
-            var firstGameNumber = random.nextInt(100);
-            var secondGameNumber = random.nextInt(100);
+            var firstGameNumber = Utils.generateNumber(MIN_VALUE, MAX_VALUE);
+            var secondGameNumber = Utils.generateNumber(MIN_VALUE, MAX_VALUE);
             var expression = firstGameNumber + " " + secondGameNumber;
             roundsData[i][0] = expression;
             roundsData[i][1] = String.valueOf(calculateGcd(firstGameNumber, secondGameNumber));
